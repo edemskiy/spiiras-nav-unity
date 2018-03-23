60f9cd9bcafd5725627b2b4999e40e90905f474d
Edemskiy Alexander <qapple@ya.ru>
2018-03-23 14:46:24 +0300

    update

--- Assets/UsersNavController.cs
+++ Assets/UsersNavController.cs
@@ -11,7 +11,7 @@ public class UsersNavController : MonoBehaviour {
         float[][] ProbArrs = new float[][]
         {
             new float[] { 0.021f, 0.002f, 0.003f, 0.005f, 0.89f, 0.072f, 0.004f, 0.003f },
-            new float[] { 0.05f, 0.005f, 0.04f, 0.9f, 0.005f }
+            new float[] { 0.06f, 0.025f, 0.05f, 0.85f, 0.015f }
         };
 
         UserController[] users = GetComponentsInChildren<UserController>();
