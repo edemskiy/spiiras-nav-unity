31d499a84de95c3b96bc3650b3f6be7fd47137c3
Edemskiy Alexander <qapple@ya.ru>
2018-03-30 17:08:46 +0300

    multiple floor random movement detection

--- Assets/ObjectLocation.cs
+++ Assets/ObjectLocation.cs
@@ -1,22 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class ObjectLocation {
+public class ObjectLocation
+{
 
     public string time;
     public float x;
     public float z;
     public string userID;
 
-    public ObjectLocation(string time, float x, float z, string userID) {
-        this.x = x;
-        this.z = z;
+    public ObjectLocation(string time, float x, float z, string userID)
+    {
+        this.x = (float)System.Math.Round((double)x, 2);
+        this.z = (float)System.Math.Round((double)z, 2);
         this.time = time;
         this.userID = userID;
     }
 
-    public string toJSON() {
+    public string toJSON()
+    {
         return JsonUtility.ToJson(this);
     }
 }
